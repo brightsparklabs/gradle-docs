b1f2f7c165fdc207bfbb6a4583bab2619ab266d1
Benjamin Wiffen <benjamin.wiffen@brightsparklabs.com>
2025-10-17 15:23:23 +1030

    RAD-241: Make Dockerfile external and modify to use nodejs base during website build

--- src/main/groovy/com/brightsparklabs/gradle/docs/DocsPlugin.groovy
+++ src/main/groovy/com/brightsparklabs/gradle/docs/DocsPlugin.groovy
@@ -21,6 +21,8 @@ import java.nio.file.Paths
 import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
 
+import groovy.text.SimpleTemplateEngine
+
 /**
  * The brightSPARK Labs Docs Plugin.
  */
@@ -528,157 +530,11 @@ class DocsPlugin implements Plugin<Project> {
                     outputfile.text = fileContent
                 }
 
-                def dockerFileContent = """
-                # -----------------------------------------
-                # BUILD STAGE: GENERATE PDF FILES
-                # -----------------------------------------
-
-                # This stage is not actually used in the website generation. It is just here to
-                # allow PDF files to be build in a container, with Graphviz+Vega diagramming tools
-                # available.
-                #
-                # Can be called manually via:
-                #
-                #   docker build -t docs -f <GENERATED_DOCKER_FILE> --target builder-pdf .
-
-                FROM eclipse-temurin:17.0.9_9-jdk-jammy as builder-pdf
-
-                RUN apt update
-                # Allow gradle-docs plugin to read git details on files.
-                RUN apt install -y git
-
-                # Allow asciidoctor-diagram to render plantuml (dot) diagrams.
-                RUN apt install -y graphviz
-
-                # Allow asciidoctor-diagram to render Vega diagrams.
-                # nvm needs an interactive shell: https://stackoverflow.com/a/60137919
-                SHELL ["/bin/bash", "--login", "-i", "-c"]
-                RUN curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-                RUN source ~/.bashrc
-                RUN nvm install lts/iron
-                RUN npm install -g vega-cli
-
-                # Get gradle distribution (done separately so Docker caches layer)
-                WORKDIR /src
-                COPY build.gradle settings.gradle gradlew ./
-                COPY gradle ./gradle/
-                RUN ./gradlew build || return 0 # force sucess as build is expected to fail due to no sources
-
-                COPY .git ./.git
-                COPY src ./src
-
-                # Build the PDF files.
-                RUN ./gradlew bslAsciidoctorPdfVersioned --no-daemon
-
-                # -----------------------------------------
-                # EXPORT STAGE
-                # -----------------------------------------
-
-                # The image produced by this stage can be used if the files are to be exported to
-                # the file system via: `docker build ... --target pdf-export-stage --output output/`
-
-                # Base off scratch so output only contains the PDF files.
-                FROM scratch AS pdf-export-stage
-                COPY --from=builder-pdf /src/build/docs/asciidoc .
-
-                # -----------------------------------------
-                # BUILD STAGE: GENERATE ASCIIDOC FILES
-                # -----------------------------------------
-
-                FROM eclipse-temurin:17.0.9_9-jdk-alpine as builder-java
-
-                # Allow gradle-docs plugin to read git details on files.
-                RUN apk add git
-
-                # Get gradle distribution (done separately so Docker caches layer)
-                WORKDIR /src
-                COPY build.gradle settings.gradle gradlew ./
-                COPY gradle ./gradle
-                RUN ./gradlew build || return 0 # force sucess as build is expected to fail due to no sources
-
-                COPY .git ./.git
-                COPY src ./src
-
-                # Build the asciidoc files and Dockerfile resources.
-                RUN ./gradlew jinjaPreProcess generateDockerfile --no-daemon
-
-                # Always ensure the images directory exists since it is copied in next stage.
-                RUN mkdir -p "${config.buildImagesDir}"
-
-                # -----------------------------------------
-                # BUILD STAGE: GENERATE WEBSITE
-                # -----------------------------------------
-
-                FROM jekyll/jekyll:4.2.2 as builder-jekyll
-
-                # Allow asciidoctor-diagram to render plantuml (dot) diagrams.
-                RUN apk add graphviz
-
-                # Allow asciidoctor-diagram to render Vega diagrams.
-                # Vega is an nodejs project, but it requires C header files from `giflib`.
-                RUN apk add giflib-dev
-                RUN npm install -g vega-cli
-
-                COPY --from=builder-java /src/build/brightsparklabs/docs/dockerfile/_config.yml .
-                COPY --from=builder-java /src/build/brightsparklabs/docs/dockerfile/Gemfile .
-
-                # Run a build to cache gems.
-                RUN jekyll build --verbose
-
-                COPY --from=builder-java /src/build/brightsparklabs/docs/jinjaProcessed .
-                COPY --from=builder-java "/src/${config.buildImagesDir}" images
-
-                # NOTE:
-                #
-                # If you do not specify `-d /tmp/site` it should default to `/srv/jekyll/_site`.
-                # However, this directory does not seem to get created for some reason. I.e. adding
-                # the following to the Dockerfile shows that the directory is not present:
-                #
-                #   RUN ls -al /srv/jekyll
-                #
-                # Explicitly building to `/tmp/site` fixes it.
-                RUN jekyll build --verbose -d /tmp/site
-
-                # NOTE:
-                #
-                # Need to chown it to root otherwise `jenkins` owns it, and the CI server fails next
-                # stage. See https://circleci.com/docs/2.0/high-uid-error/
-                RUN chown -R root:root /tmp/site
-
-                # -----------------------------------------
-                # EXPORT STAGE
-                # -----------------------------------------
-
-                # The image produced by this stage can be used if the files are to be exported to
-                # the file system via: `docker build ... --target export-stage --output output/`
-
-                # Base off scratch so output only contains the website files.
-                FROM scratch AS export-stage
-                COPY --from=builder-jekyll /tmp/site .
-
-                # -----------------------------------------
-                # FINAL STAGE
-                # -----------------------------------------
-
-                # The image produced by this stage hosts the website.
-
-                FROM caddy:2.9.1
-                MAINTAINER brightSPARK Labs <enquire@brightsparklabs.com>
-                ARG BUILD_DATE=UNKNOWN
-                ARG VCS_REF=UNKNOWN
-                LABEL org.label-schema.name="nswcc-documentation" \
-                      org.label-schema.description="Image used to host documentation as a static website." \
-                      org.label-schema.vendor="brightSPARK Labs" \
-                      org.label-schema.schema-version="1.0.0-rc1" \
-                      org.label-schema.vcs-url="https://bitbucket.org/brightsparklabs/gradle-docs" \
-                      org.label-schema.vcs-ref=\${VCS_REF} \
-                      org.label-schema.build-date=\${BUILD_DATE}
-                ENV META_BUILD_DATE=\${BUILD_DATE} \
-                    META_VCS_REF=\${VCS_REF}
-
-                COPY --from=builder-jekyll /tmp/site /usr/share/caddy/
-                COPY --from=builder-pdf /src/build/docs/asciidoc /usr/share/caddy/export
-                """.stripIndent().trim()
+                // Read in the Dockerfile contents and then manually perform string extrapolation.
+                def rawDockerFileContent = Resources.toString(getClass().getResource("/Dockerfile"), Charsets.UTF_8)
+                def engine = new SimpleTemplateEngine()
+                def template = engine.createTemplate(rawDockerFileContent)
+                def dockerFileContent = template.make([config: config]).toString()
 
                 def dockerFile = new File(outputDir, "Dockerfile")
                 dockerFile.text = dockerFileContent
